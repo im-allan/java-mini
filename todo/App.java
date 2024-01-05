1dd0a53ca7efc1457f18b1fac859e2639a5b7d7c
im-allan <al.silvaquiroz@gmail.com>
2024-01-04 21:48:31 -0300

    feat: Added App.java file with the instance of the class

---
+++ todo/App.java
@@ -0,0 +1,18 @@
+package todo;
+
+import java.util.List;
+import java.util.Date;
+
+public class App {
+  TaskManager gestorTareas = new TaskManager();
+
+  // Agregar una tarea
+  Task tarea = new Task("Llamar al médico", new Date(), "Pendiente");
+  gestorTareas.agregarTarea(tarea);
+
+  // Listar las tareas
+  List<Task> tareas = gestorTareas.listarTareas();
+  for (Task t : tareas) {
+      System.out.println(t.getTitulo());
+  }  
+}
